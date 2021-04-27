db80ca0a2faa1cd7709689b044f584c3c7f3766e
JustNrik <justnrik@gmail.com>
2021-04-27 02:32:26 -0400

    asd

--- Aspert/Aspert/ViewModels/MenuPageViewModel.cs
+++ Aspert/Aspert/ViewModels/MenuPageViewModel.cs
@@ -6,12 +6,12 @@ namespace Aspert.ViewModels
     public class MenuPageViewModel : ViewModel
     {
         public ICommand Back { get; }
-        public ICommand Menu { get; }
+        public ICommand Configuration { get; }
 
         public MenuPageViewModel()
         {
             Back = new Command(async () => await Application.Current.MainPage.Navigation.PushModalAsync(new MainPage()));
-            Menu = new Command(async () => await Application.Current.MainPage.Navigation.PushModalAsync(new MenuPage()));
+            Configuration = new Command(async () => await Application.Current.MainPage.Navigation.PushModalAsync(new ConfigurationPage()));
         }
     }
 }
