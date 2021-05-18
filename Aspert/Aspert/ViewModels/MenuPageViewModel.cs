78fbf52cbbb8d1226435cff08131302d6844cfc9
JustNrik <justnrik@gmail.com>
2021-05-18 00:34:05 -0400

    x

--- Aspert/Aspert/ViewModels/MenuPageViewModel.cs
+++ Aspert/Aspert/ViewModels/MenuPageViewModel.cs
@@ -3,18 +3,14 @@ using Xamarin.Forms;
 
 namespace Aspert.ViewModels
 {
-    public class MenuPageViewModel : ViewModel
+    public class MenuPageViewModel : PageViewModel
     {
-        public ICommand Back { get; }
-        public ICommand Configuration { get; }
         public ICommand NewsPage { get; }
         public ICommand AbcPage { get; }
         public ICommand BookPage { get; }
 
         public MenuPageViewModel()
         {
-            Back = new Command(async () => await Application.Current.MainPage.Navigation.PushModalAsync(new MainPage()));
-            Configuration = new Command(async () => await Application.Current.MainPage.Navigation.PushModalAsync(new ConfigurationPage()));
             NewsPage = new Command(async () => await Application.Current.MainPage.Navigation.PushModalAsync(new NewsPage()));
             AbcPage = new Command(async () => await Application.Current.MainPage.Navigation.PushModalAsync(new AbcPage()));
             BookPage = new Command(async () => await Application.Current.MainPage.Navigation.PushModalAsync(new BookPage()));
---
+++ Aspert/Aspert/ViewModels/PageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Aspert.ViewModels
+{
+    public class PageViewModel : ViewModel
+    {
+        public ICommand Back { get; }
+        public ICommand Configuration { get; }
+
+        public PageViewModel()
+        {
+            Back = new Command(async () => await Application.Current.MainPage.Navigation.PushModalAsync(new MainPage()));
+            Configuration = new Command(async () => await Application.Current.MainPage.Navigation.PushModalAsync(new ConfigurationPage()));
+        }
+    }
+}
